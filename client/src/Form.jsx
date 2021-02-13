e72e2d2b0c12b706b524e310b78b819ef237e498
AlexShold <ashold12@gmail.com>
2021-02-13 13:22:27 -0600

    added form text state management

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -4,14 +4,17 @@ class Form extends React.Component {
   constructor (props){
     super(props);
     this.state = {
-      item: '',
-      count: '',
+      'item': '',
+      'count': '',
     }
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  setStateString(e, state) {
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
     this.setState({
-      state: e.target.value
+      [name]: value
     })
   }
 
@@ -19,10 +22,10 @@ render() {
   return (
     <form>
       <label> Item:
-        <input name="item" value= {this.state.item}/>
+        <input name="item" value= {this.state.item} onChange ={this.handleChange}/>
       </label>
       <label> Count:
-        <input name="count" value={this.state.count}/>
+        <input name="count" value={this.state.count} onChange = {this.handleChange}/>
       </label>
       <button>Add Grocery</button>
     </form>
