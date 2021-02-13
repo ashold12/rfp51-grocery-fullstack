95799bc802766a5c80481c40a0f30937b75769fa
AlexShold <ashold12@gmail.com>
2021-02-13 12:26:40 -0600

    added image and div container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,18 @@ import React from "react";
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      list: [{item: 'potater', count: 2}, {item: 'banan', count:4}]
+    }
   }
 
   render () {
-    return <h1> hi guys its me </h1>
+    return(
+      <div>
+        <img src="grocery-bags.png"/>
+        <h1> hi guys its me </h1>
+      </div>
+    )
   }
 }
 
