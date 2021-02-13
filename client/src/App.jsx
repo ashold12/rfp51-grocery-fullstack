2eed6c06871c2e3f424af168df59ca9e97ad0afb
AlexShold <ashold12@gmail.com>
2021-02-13 13:04:51 -0600

    updated form to stateful component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import List from './List.jsx';
+import Form from './Form.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      list: [{item: 'potater', count: 2}, {item: 'banan', count:4}]
+      list: [{item: 'potater', count: 2}, {item: 'banan', count:4}],
+      formItem: '',
+      formQuantity: '',
     }
   }
 
@@ -14,6 +17,7 @@ class App extends React.Component {
       <div>
         <img src="grocery-bags.png"/>
         <h1> I heard you guys like Groceries </h1>
+        <Form></Form>
         <List items = {this.state.list}></List>
       </div>
     )
--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,1 +1,33 @@
 import React from "react";
+
+class Form extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      item: '',
+      count: '',
+    }
+  }
+
+  setStateString(e, state) {
+    this.setState({
+      state: e.target.value
+    })
+  }
+
+render() {
+  return (
+    <form>
+      <label> Item:
+        <input name="item" value= {this.state.item}/>
+      </label>
+      <label> Count:
+        <input name="count" value={this.state.count}/>
+      </label>
+      <button>Add Grocery</button>
+    </form>
+  );
+  }
+}
+
+export default Form
