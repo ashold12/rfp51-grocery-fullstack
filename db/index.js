b27562045247b31e56d9e905837b853b46891c40
AlexShold <ashold12@gmail.com>
2021-02-13 14:03:00 -0600

    created groceryList db/schema and connected to server

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   user: 'student',
   password: 'student',
-  database: "",
+  database: "groceryList",
 });
 
 connection.connect();
--- schema.sql
+++ schema.sql
@@ -2,8 +2,11 @@
  *    sqlite3 items.db < schema.sql
  *  to create the database and the tables.*/
 
+USE groceryList;
+
 CREATE TABLE groceryList (
   id INT NOT NULL AUTO_INCREMENT,
   name varchar(100) NOT NUll,
-  count INT NOT NULL
+  count INT NOT NULL,
+  PRIMARY KEY (id)
 );
