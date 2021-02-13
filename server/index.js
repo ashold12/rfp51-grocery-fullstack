dfba7dcf5752d1e20cbe3ed48c37cc13dd671862
AlexShold <ashold12@gmail.com>
2021-02-13 14:10:02 -0600

    working get request

--- server/index.js
+++ server/index.js
@@ -9,8 +9,16 @@ const db = require("../db/index.js");
 app.use(cors());
 
 
-app.get('/', (req, res) =>
-  res.send('Get success'));
+app.get('/', (req, res) => {
+  db.query('SELECT name, count FROM groceryList', (err, data) => {
+    if (err) {
+      res.status(404).send(err)
+    } else {
+      console.log('query successful')
+      res.status(201).send(data)
+    }
+  });
+});
 
 app.post('/', (req, res) =>
   res.send('Post Success'));
