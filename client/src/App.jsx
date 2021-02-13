5e77e74b0340d84af2bf31a9c92fb65ba1d488cd
AlexShold <ashold12@gmail.com>
2021-02-13 12:20:35 -0600

    made index file as container for app | skeleton for app class

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render () {
+    return <h1> hi guys its me </h1>
+  }
+}
+
+export default App
+
+
+
